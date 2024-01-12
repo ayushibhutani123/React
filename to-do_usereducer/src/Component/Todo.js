c71088d5272673b3000ffa3dc30f74f580ffa1de
Ayushi <bhutaniayushi0072@gmail.com>
2024-01-12 12:17:34 +0530

    commit3

--- to-do_usereducer/src/Component/Todo.js
+++ to-do_usereducer/src/Component/Todo.js
@@ -1,53 +1,83 @@
-import React,{useReducer} from "react";
+import React, { useReducer } from "react";
 import "./Todo.css";
-export default function Todo() {
-    const Changereducer=(state,action)=>{
-switch(action.type){
-    case "buttonclick":
-    return {...state,value:action.value};
-    case "addTodo":
-        const updatedItems= [...state.items, state.value];
-        const updatedItemsstring=JSON.stringify(updatedItems);
 
-        localStorage.setItem('user',updatedItemsstring)
-        return{...state,items:updatedItems , value: "" }
-    default:
+export default function Todo() {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "Setvalue":
+        return { ...state, value: action.value };
+      case "ButtonClicked":
+        const updatedItems = [...state.items, state.value];
+        localStorage.setItem("todoItems", JSON.stringify(updatedItems));
+        return {
+          ...state,
+          items: updatedItems,
+          value: "",
+        };
+        case "prevClick":
+          const storedItems = JSON.parse(localStorage.getItem("todoItems")) || [];
+        return {
+          ...state,
+          items: storedItems,
+          value: "",
+        };
+      default:
         return state;
-}
     }
-    // const[inputText,setInputtext]=useState("");
-    const[state,dispatch]=useReducer(Changereducer,{value:"",items:[]})
-    const handleonChange=(event)=>{
-        console.log(event.target.value)
-        dispatch({
-            type:"buttonclick",
-            value:event.target.value
-        })
-            }
-            const handleonClick=(event)=>{
-                const storedItems = localStorage.getItem('user');
-    if (storedItems) {
-dispatch({
-    type:"addTodo"
-})
-            }}
-            console.log("Updated items array:", state.items);
+  };
+
+  const initialState = {
+    value: "",
+    items: [],
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleInputChange = (event) => {
+    dispatch({
+      type: "Setvalue",
+      value: event.target.value,
+    });
+  };
+
+  const handleOnClick = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: "ButtonClicked"
+    });
+  };
+const handleonprevClick=(event)=>{
+  dispatch({
+    type:"prevClick"
+  });
+}
+
   return (
     <>
       <div className="container">
-        <label forname="Todo" className="todo">
+        <label htmlFor="Todo" className="todo">
           To Do List:
         </label>
-        <br></br>
-        <input type="text" className="inputtodo" value={state.value} onChange={handleonChange} ></input><br></br>
-<button className="btn-success" onClick={handleonClick}>Add</button>
+        <br />
+        <input
+          type="text"
+          className="inputtodo"
+          value={state.value}
+          onChange={handleInputChange}
+        />
+        <br />
+        <button className="btn-success" onClick={handleOnClick}>
+          Add
+        </button>
+        
+        <button className="btn-success" onClick={handleonprevClick}>Preview Previous</button>
       </div>
       <div className="bodycontainer">
-<ul>
-{state.items.map((item, index) => (
+        <ul>
+          {state.items.map((item, index) => (
             <li key={index}>{item}</li>
           ))}
-</ul>
+        </ul>
       </div>
     </>
   );
